16e4ad6648c6308671211b16e3e84e5ac0beb8a9
Maryna Huhlia <marynahuhlja@gmail.com>
2023-09-04 16:30:29 +0300

    n

--- test/main.tf
+++ test/main.tf
@@ -4,95 +4,22 @@ provider "aws" {
   shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 
-####Create VPC and IGW ####
-data "aws_availability_zones" "available" {}
-
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "${var.env}-vpc"
-  }
+module "vpc_dev" {
+  source               = "../modules/aws_network"
+  env                  = "development"
+  vpc_cidr             = "10.100.0.0/16"
+  public_subnet_cidrs  = ["10.100.1.0/24", "10.100.2.0/24"]
+  private_subnet_cidrs = ["10.100.11.0/24", "10.100.22.0/24"]
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id ##attaching to VPC "main"
-  tags = {
-    Name = "${var.env}-igw"
-  }
+########################
+
+output "dev_public_subnet_ids" {
+  value = module.vpc_dev.public_subnet_ids
 
 }
-#### Public Subnet and Routing ####
-resource "aws_subnet" "public_subnets" {
-  count                   = length(var.public_subnet_cidrs) #  поскольку в var.public_subnet_cidrs пррписани 2 сабнета, то создаст 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = element(var.public_subnet_cidrs, count.index)            #берет єлемент сайдер блоков 
-  availability_zone       = data.aws_availability_zones.available.names[count.index] # проверяет
-  map_public_ip_on_launch = true                                                     #делает чтоб получали public IP 
-  tags = {
-    Name = "${var.env}-public-${count.index + 1}"
-  }
-}
-resource "aws_route_table" "public_subnets" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = "${var.env}-route-public-subnets"
-  }
-}
 
-#### Associate route-table with all subnets ####
-resource "aws_route_table_association" "public_routes" {
-  count          = length(aws_subnet.public_subnets[*].id)
-  route_table_id = aws_route_table.public_subnets.id
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-}
+output "dev_private_subnet_ids" {
+  value = module.vpc_dev.private_subnet_ids
 
-##### NAT Gateway with Elastic IPs####
-resource "aws_eip" "nat" {
-  count = length(var.private_subnet_cidrs)
-  vpc   = true
-  tags = {
-    Name = "${var.env}-nat-gw-${count.index + 1}"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  count         = length(var.private_subnet_cidrs)
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
-  tags = {
-    Name = "${var.env}-nat-gw-${count.index + 1}"
-  }
-
-}
-#### Private Subnets and Routing ####
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnet_cidrs) #  поскольку в var.public_subnet_cidrs пррписани 2 сабнета, то создаст 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.private_subnet_cidrs, count.index)           #берет єлемент сайдер блоков 
-  availability_zone = data.aws_availability_zones.available.names[count.index] # проверяет
-  tags = {
-    Name = "${var.env}-private-${count.index + 1}"
-  }
-}
-
-resource "aws_route_table" "private_subnets" {
-  count  = length(var.private_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[count.index].id
-  }
-  tags = {
-    Name = "${var.env}-route-private-subnet-${count.index + 1}"
-  }
-}
-
-resource "aws_route_table_association" "private_routes" {
-  count          = length(aws_subnet.private_subnets[*].id)
-  route_table_id = aws_route_table.private_subnets[count.index].id
-  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
 }
