fea14cf0e6138d683f7c00438be494626fd3ec45
Maryna Huhlia <marynahuhlja@gmail.com>
2023-09-06 14:45:37 +0300

    fix rep

--- environment/test/ecr/main.tf
+++ environment/test/ecr/main.tf
@@ -1,18 +1,18 @@
 module "my_ecr" {
-  source           = "../../modules/ecr"
-  repository_name  = "Project/environment/test/ecr"
+  source          = "../../../modules/ecr"
+  repository_name = "project-test"
   lifecycle_policy = jsonencode({
     rules = [
       {
-        rulePriority    = 1,
-        description     = "Keep last 30 images",
-        selection       = {
-          tagStatus    = "untagged",
-          countType    = "sinceImagePushed",
-          countNumber  = 30,
-          countUnit    = "days",
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countNumber = 30,
+          countUnit   = "days",
         },
-        action          = {
+        action = {
           type = "expire"
         }
       }
