da1ecbc811b8b7d02903431fda67c7893bc1f9ec
MarynaHuhlja <132301794+MarynaHuhlja@users.noreply.github.com>
2023-09-06 12:28:46 +0300

    Update main.tf

--- environment/test/ecs/main.tf
+++ environment/test/ecs/main.tf
@@ -1,1 +1,21 @@
-
+module "my_ecr" {
+  source           = "./my-ecr-module"
+  repository_name  = "my-container-repo"
+  lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority    = 1,
+        description     = "Keep last 30 images",
+        selection       = {
+          tagStatus    = "untagged",
+          countType    = "sinceImagePushed",
+          countNumber  = 30,
+          countUnit    = "days",
+        },
+        action          = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
