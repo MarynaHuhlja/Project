33a7c7eabd812a5d33d277561e43561d413c76fb
MarynaHuhlja <132301794+MarynaHuhlja@users.noreply.github.com>
2023-09-06 12:09:25 +0300

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,1 +1,9 @@
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = var.repository_name
+}
+
+resource "aws_ecr_lifecycle_policy" "my_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.my_ecr_repo.name
+  policy     = var.lifecycle_policy
+}
 
