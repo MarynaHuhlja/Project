641b3045a12379ed70cf752991310bd69731dac9
MarynaHuhlja <132301794+MarynaHuhlja@users.noreply.github.com>
2023-09-08 16:29:50 +0300

    Create main.tf

---
+++ environment/test/ecs/main.tf
@@ -0,0 +1,321 @@
+#### remore state ECR ###
+terraform {
+  backend "s3" {
+    bucket = "maryna-terraform-state"
+    key    = "environment/test/ecs/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "maryna-terraform-state"
+    key    = "environment/test/vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  region = "eu-central-1"
+  name   = "test-${basename(path.cwd)}"
+  azs    = slice(data.aws_availability_zones.available.names, 0, 1)
+
+  container_name = "nginx"
+  container_port = 80
+
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/terraform-aws-modules/terraform-aws-ecs"
+  }
+}
+
+################################################################################
+# Cluster
+################################################################################
+
+module "ecs_cluster" {
+  source = "terraform-aws-modules/ecs/aws//modules/cluster"
+
+  cluster_name = local.name
+
+  # Capacity provider - autoscaling groups
+  default_capacity_provider_use_fargate = false
+  autoscaling_capacity_providers = {
+    # On-demand instances
+    ex-1 = {
+      auto_scaling_group_arn         = module.autoscaling["ex-1"].autoscaling_group_arn
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 5
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 60
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 60
+        base   = 20
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Service
+################################################################################
+
+module "ecs_service" {
+  source = "terraform-aws-modules/ecs/aws//modules/service"
+
+  # Service
+  name        = local.name
+  cluster_arn = module.ecs_cluster.arn
+
+  memory = 256
+
+  # Task Definition
+  requires_compatibilities = ["EC2"]
+  capacity_provider_strategy = {
+    # On-demand instances
+    ex-1 = {
+      capacity_provider = module.ecs_cluster.autoscaling_capacity_providers["ex-1"].name
+      weight            = 1
+      base              = 1
+    }
+  }
+
+  volume = {
+    my-vol = {}
+  }
+
+  # Container definition(s)
+  container_definitions = {
+    (local.container_name) = {
+      image = "653663230212.dkr.ecr.eu-central-1.amazonaws.com/nginx:v1"
+
+      memory = 256
+      port_mappings = [
+        {
+          name          = local.container_name
+          containerPort = local.container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      mount_points = [
+        {
+          sourceVolume  = "dev-vol",
+          containerPath = "/var/www/dev-vol"
+        }
+      ]
+
+      entry_point = ["/usr/sbin/apache2", "-D", "FOREGROUND"]
+
+      # Example image used requires access to write to root filesystem
+      readonly_root_filesystem = false
+    }
+  }
+
+  load_balancer = {
+    service = {
+      target_group_arn = element(module.alb.target_group_arns, 0)
+      container_name   = local.container_name
+      container_port   = local.container_port
+    }
+  }
+
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  security_group_rules = {
+    alb_http_ingress = {
+      type                     = "ingress"
+      from_port                = local.container_port
+      to_port                  = local.container_port
+      protocol                 = "tcp"
+      description              = "Service port"
+      source_security_group_id = module.alb_sg.security_group_id
+    }
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+}
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-service"
+  description = "Service security group"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress_rules       = ["http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = local.tags
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = local.name
+
+  load_balancer_type = "application"
+
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnets         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  security_groups = [module.alb_sg.security_group_id]
+
+  http_tcp_listeners = [
+    {
+      port               = local.container_port
+      protocol           = "HTTP"
+      target_group_index = 0
+    },
+  ]
+
+  target_groups = [
+    {
+      name             = "${local.name}-${local.container_name}"
+      backend_protocol = "HTTP"
+      backend_port     = local.container_port
+      target_type      = "ip"
+    },
+  ]
+
+  tags = local.tags
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 6.5"
+
+  for_each = {
+    # On-demand instances
+    ex-1 = {
+      instance_type              = "t2.micro"
+      use_mixed_instances_policy = false
+      mixed_instances_policy     = {}
+      user_data                  = <<-EOT
+        #!/bin/bash
+        cat <<'EOF' >> /etc/ecs/ecs.config
+        ECS_CLUSTER=${local.name}
+        ECS_LOGLEVEL=debug
+        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
+        ECS_ENABLE_TASK_IAM_ROLE=true
+        EOF
+      EOT
+    }
+    # # Spot instances
+    # ex-2 = {
+    #   instance_type              = "t3.medium"
+    #   use_mixed_instances_policy = true
+    #   mixed_instances_policy = {
+    #     instances_distribution = {
+    #       on_demand_base_capacity                  = 0
+    #       on_demand_percentage_above_base_capacity = 0
+    #       spot_allocation_strategy                 = "price-capacity-optimized"
+    #     }
+
+    #     override = [
+    #       {
+    #         instance_type     = "t2.micro"
+    #         weighted_capacity = "2"
+    #       },
+    #       {
+    #         instance_type     = "t3.large"
+    #         weighted_capacity = "1"
+    #       },
+    #     ]
+    #   }
+    #   user_data = <<-EOT
+    #     #!/bin/bash
+    #     cat <<'EOF' >> /etc/ecs/ecs.config
+    #     ECS_CLUSTER=${local.name}
+    #     ECS_LOGLEVEL=debug
+    #     ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
+    #     ECS_ENABLE_TASK_IAM_ROLE=true
+    #     ECS_ENABLE_SPOT_INSTANCE_DRAINING=true
+    #     EOF
+    #   EOT
+    # }
+  }
+
+  name = "${local.name}-${each.key}"
+
+  image_id      = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
+  instance_type = each.value.instance_type
+
+  security_groups                 = [module.autoscaling_sg.security_group_id]
+  user_data                       = base64encode(each.value.user_data)
+  ignore_desired_capacity_changes = true
+
+  create_iam_instance_profile = true
+  iam_role_name               = local.name
+  iam_role_description        = "ECS role for ${local.name}"
+  iam_role_policies = {
+    AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+    AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  vpc_zone_identifier = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  health_check_type   = "EC2"
+  min_size            = 1
+  max_size            = 1
+  desired_capacity    = 1
+
+  # https://github.com/hashicorp/terraform-provider-aws/issues/12582
+  autoscaling_group_tags = {
+    AmazonECSManaged = true
+  }
+
+  # Required for  managed_termination_protection = "ENABLED"
+  protect_from_scale_in = true
+
+  # Spot instances
+  use_mixed_instances_policy = each.value.use_mixed_instances_policy
+  mixed_instances_policy     = each.value.mixed_instances_policy
+
+  tags = local.tags
+}
+
+module "autoscaling_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = local.name
+  description = "Autoscaling group security group"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.alb_sg.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+
+  egress_rules = ["all-all"]
+
+  tags = local.tags
+}
